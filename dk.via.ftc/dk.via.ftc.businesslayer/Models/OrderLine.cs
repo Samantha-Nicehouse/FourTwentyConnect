334c217538d68a7785842160413e17557e70ebc4
Uldis Alksnis <csmccs1@gmail.com>
2020-12-01 17:31:36 +0100

    Merge Request Fix for Strain API v1

--- dk.via.ftc/dk.via.ftc.businesslayer/Models/OrderLine.cs
+++ dk.via.ftc/dk.via.ftc.businesslayer/Models/OrderLine.cs
@@ -1,27 +1,21 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.Text.Json.Serialization;
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
 
 namespace dk.via.ftc.businesslayer.Models
 {
     public partial class Orderline
     {
-        [JsonPropertyName("orderline_id"), Key]
         public int OrderlineId { get; set; }
-        [JsonPropertyName("price")]
         public decimal Price { get; set; }
-        [JsonPropertyName("quantity")]
         public int? Quantity { get; set; }
-        
-        [JsonPropertyName("vendor_id")]
         public string VendorId { get; set; }
-        [JsonPropertyName("product_id")]
         public int ProductId { get; set; }
-        [JsonPropertyName("order_id")]
         public int OrderId { get; set; }
 
-        public virtual PurchaseOrder Order { get; set; }
+        //public virtual PurchaseOrder Order { get; set; }
         public virtual Product Product { get; set; }
         public virtual Vendor Vendor { get; set; }
     }
 }
-
